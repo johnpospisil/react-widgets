79a283e0500e4e59b3d7491ba872c3870b810a33
johnpospisil <audiofreak7@gmail.com>
2020-09-15 12:24:53 -0700

    Create the Accordian component and add questions

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 import React from "react";
+import Accordion from "./components/Accordion";
+
+const items = [
+  {
+    title: "What is React?",
+    content: "React is a front end javascript framework",
+  },
+  {
+    title: "Why use React?",
+    content:
+      "React is a favorite JS library among engineers all over the globe",
+  },
+  {
+    title: "How do you use React?",
+    content: "You use React by creating components",
+  },
+];
 
 export default () => {
-  return <div>Widget App!</div>;
+  return (
+    <div>
+      <h2>The App Component!</h2>
+      <Accordion items={items} />
+    </div>
+  );
 };
