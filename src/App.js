01b13bd9df1f1c299c03f454d4b6c32012455d7e
johnpospisil <audiofreak7@gmail.com>
2020-09-15 17:04:21 -0700

    Create the Search component.Comment out Accordion

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
 import React from "react";
-import "semantic-ui-css/semantic.min.css";
-import Accordion from "./components/Accordion";
+// import "semantic-ui-css/semantic.min.css";
+// import Accordion from "./components/Accordion";
+import Search from "./components/Search";
 
-const items = [
-  {
-    title: "What is React?",
-    content: "React is a front end javascript framework",
-  },
-  {
-    title: "Why use React?",
-    content:
-      "React is a favorite JS library among engineers all over the globe",
-  },
-  {
-    title: "How do you use React?",
-    content: "You use React by creating components",
-  },
-];
+// const items = [
+//   {
+//     title: "What is React?",
+//     content: "React is a front end javascript framework",
+//   },
+//   {
+//     title: "Why use React?",
+//     content:
+//       "React is a favorite JS library among engineers all over the globe",
+//   },
+//   {
+//     title: "How do you use React?",
+//     content: "You use React by creating components",
+//   },
+// ];
 
 export default () => {
   return (
     <div>
-      <Accordion items={items} />
+      {/* <Accordion items={items} /> */}
+      <Search />
     </div>
   );
 };
