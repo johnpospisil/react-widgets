5523a9dbaf5eb60da987b8d2b234169ba4a2fd9a
johnpospisil <audiofreak7@gmail.com>
2020-09-19 19:47:24 -0700

    Send GET requests to search wikipedia

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,23 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Search = () => {
   const [term, setTerm] = useState("");
 
-  console.log("I run with every render");
-
   useEffect(() => {
-    console.log(
-      "I run at initial render, OR (whenever it rerenders AND some piece of data has changed)."
-    );
+    const search = async () => {
+      await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+    };
+
+    search();
   }, [term]);
 
   return (
