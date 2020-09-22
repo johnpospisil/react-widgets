5e75e67961b28fe3f69d24224230a6cc8d06e9f3
johnpospisil <audiofreak7@gmail.com>
2020-09-22 13:00:38 -0700

    Create a basic 500ms delay for user searches

--- src/components/Search.js
+++ src/components/Search.js
@@ -22,9 +22,11 @@ const Search = () => {
       setResults(data.query.search);
     };
 
-    if (term) {
-      search();
-    }
+    setTimeout(() => {
+      if (term) {
+        search();
+      }
+    }, 500);
   }, [term]);
 
   const renderedResult = results.map((result) => {
