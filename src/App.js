86a87951223e3b97f50f38a07468c79eb3b82b78
johnpospisil <audiofreak7@gmail.com>
2020-09-23 14:27:59 -0700

    Create the basic Dropdown component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import "semantic-ui-css/semantic.min.css";
 // import Accordion from "./components/Accordion";
-import Search from "./components/Search";
+// import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 
 // const items = [
 //   {
@@ -19,11 +20,27 @@ import Search from "./components/Search";
 //   },
 // ];
 
+const options = [
+  {
+    label: "The Color Red",
+    value: "red",
+  },
+  {
+    label: "The Color Green",
+    value: "green",
+  },
+  {
+    label: "The Color Blue",
+    value: "blue",
+  },
+];
+
 export default () => {
   return (
     <div>
       {/* <Accordion items={items} /> */}
-      <Search />
+      {/* <Search /> */}
+      <Dropdown options={options} />
     </div>
   );
 };
