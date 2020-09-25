8c102e76014721aa91bb98cf4a7da3ca392f4554
johnpospisil <audiofreak7@gmail.com>
2020-09-25 12:45:15 -0700

    Add the user text input field

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "semantic-ui-css/semantic.min.css";
 // import Accordion from "./components/Accordion";
 // import Search from "./components/Search";
