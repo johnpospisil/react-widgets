be9a3c5a422af5b4ea3f9c499f015e8c8f0ef2f5
johnpospisil <audiofreak7@gmail.com>
2020-09-27 11:29:01 -0700

    Add axios post requests to google's translate API

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,8 +1,21 @@
-import React, { useState, useEffect } from "react";
+/* eslint-disable no-undef */
+import React, { useEffect } from "react";
+import axios from "axios";
 
 const Convert = ({ language, text }) => {
   useEffect(() => {
-    console.log("New language or text detected!");
+    // console.log("New language or text detected!");
+    axios.post(
+      "https://translation.googleapis.com/language/translate/v2",
+      {},
+      {
+        params: {
+          q: text,
+          target: language.value,
+          key: "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM",
+        },
+      }
+    );
   }, [language, text]);
   return <div />;
 };
